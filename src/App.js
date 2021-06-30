135acf6432ea4797213418ee72cd8661a1a8c891
Eilon Klein <eilonlon.klein@gmail.com>
2021-07-01 00:14:07 +0300

    Added console log to the fetch data - now it's working

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);
       const data = response.json();
+      console.log(data);
     } catch (error) {
       console.log(error);
     }
