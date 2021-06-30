ea6d741feff0fdda6ae2787be06fb65bf21fa814
Eilon Klein <eilonlon.klein@gmail.com>
2021-07-01 00:21:32 +0300

    Moved OpenWeather API Key to separated JSON file

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import Location from './Components/Location';
 import CurrentWeather from './Components/CurrentWeather';
 import FutureForcast from './Components/FutureForcast';
-
-const API_KEY = '1455c00b67b0bcfef51d2885223f7299';
+import APIKey from './API_KEY.json'
 
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
 
   async function fetchCurrentWeather(city) {
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`);
       const data = response.json();
       console.log(data);
     } catch (error) {
